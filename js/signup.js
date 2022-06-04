7c3c9507df00d99d09e92ef2407045753bfa69ae
ilysenko <i.lysenko.bekey@gmail.com>
2022-06-04 13:04:41 +0300

    added signup

---
+++ js/signup.js
@@ -0,0 +1,13 @@
+const signInBtn = document.querySelector('.signin-btn');
+const signUnBtn = document.querySelector('.signup-btn');
+const formBox = document.querySelector('.form-box');
+const body = document.body;
+
+signInBtn.addEventListener('click', function () {
+    formBox.classList.remove('active');
+    body.classList.remove('active');
+});
+signUnBtn.addEventListener('click', function () {
+    formBox.classList.add('active');
+    body.classList.add('active');
+});

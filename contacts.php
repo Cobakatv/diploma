56576c4b4e33de9880bb0e24384b6ba325552b42
Alexander Solomakha <solomahaa348@gmail.com>
2022-06-06 18:36:41 +0300

    contacts.php

--- contacts.php
+++ contacts.php
@@ -5,6 +5,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="apple-touch-icon" sizes="114x114" href="img/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="img/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="img/favicons/favicon-16x16.png">
+    <link rel="manifest" href="img/favicons/site.webmanifest">
+    <link rel="mask-icon" href="img/favicons/safari-pinned-tab.svg" color="#7f452f">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="theme-color" content="#ffffff">
+
     <link rel="stylesheet" href="/css/normalize.css">
     <link rel="stylesheet" href="/css/style.css">
     <link rel="stylesheet" href="/css/contacts.css">
@@ -127,7 +136,8 @@
                         <h3 class="contact-title">Как нас найти?</h3>
                         <p class="contact-text">Адрес нашей компании: <strong>ул. Академика Проскуры, 10</strong></p>
                         <p class="contact-text">Телефоны нашей компании:
-                             <span class="contacts-color"><a class="contact-tell" href="tel:380981927420">+(380) 98-192-74-20</a>,<a class="contact-tell" href="tel:380971256912">+(380) 97-125-69-12</a></span></p>
+                            <span class="contacts-color"><a class="contact-tell" href="tel:380981927420">+(380) 98-192-74-20</a>,<a class="contact-tell" href="tel:380971256912">+(380) 97-125-69-12</a></span>
+                        </p>
                     </div>
                     <div class="return">
                         <h3 class="contact-title">Пункты самовывоза и возврата?</h3>
@@ -135,7 +145,7 @@
                         <p class="contact-text">Пункты приема возвратов не имеют сервисных специалистов. Окончательное решение по возврату товара будет принято после осмотра товара специалистом сервиса.</p>
                         <iframe class="iframe" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2349.577881162745!2d36.28525386296048!3d50.04447023359722!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4127a6e2ef582b59%3A0x1f4a90274bfcd858!2z0YPQuy4g0JDQutCw0LTQtdC80LjQutCwINCf0YDQvtGB0LrRg9GA0YssIDEwLCDQpdCw0YDRjNC60L7Qsiwg0KXQsNGA0YzQutC-0LLRgdC60LDRjyDQvtCx0LvQsNGB0YLRjCwgNjEwMDA!5e0!3m2!1sru!2sua!4v1654525451196!5m2!1sru!2sua" width="700" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                     </div>
-                    
+
                 </div>
             </div>
         </div>
@@ -172,4 +182,5 @@
 
     <script src="/js/main.js"></script>
 </body>
+
 </html>

f9437c5b0d7a63619f33ad47c11a48fb86b58e0b
Alexander Solomakha <solomahaa348@gmail.com>
2022-06-10 14:37:28 +0300

    js/signup.js

--- js/signup.js
+++ js/signup.js
@@ -1,3 +1,4 @@
+
 const signInBtn = document.querySelector('.signin-btn');
 const signUnBtn = document.querySelector('.signup-btn');
 const formBox = document.querySelector('.form-box');
@@ -13,20 +14,35 @@ signUnBtn.addEventListener('click', function () {
 });
 
 
-// registration
-const formRegistration = document.forms["registration"];
+// // registration
+
+
 let registerUserPassword = document.querySelector(".registerUserPassword");
 let repeatregisterUserPassword = document.querySelector(".repeatregisterUserPassword");
 
-    registerUserPassword.oninput = function() {
-    this.value = this.value.substr(0, 8);
-}
-    repeatregisterUserPassword.oninput = function() {
-    this.value = this.value.substr(0, 8);
-}
-   
+//     registerUserPassword.oninput = function() {
+//         this.value = this.value.substr(0, 8);
+// }
+//     repeatregisterUserPassword.oninput = function() {
+//     this.value = this.value.substr(0, 8);
+// }
 
+
+// registration
+const formRegistration = document.forms["registration"];
+const formArr = Array.from(registration);
+const validFormArr = [];
+const button = registration.elements["button"];
+
+formArr.forEach((el) => {
+    if (el.hasAttribute("data-reg")) {
+        el.setAttribute("is-valid", "0");
+        validFormArr.push(el);
+    }
+})
 formRegistration.addEventListener("input", inputHandler);
+button.addEventListener("click", buttonHandler);
+
 
 function inputHandler({target}) {
     if (target.hasAttribute("data-reg")) {
@@ -39,29 +55,27 @@ function inputCheck(el) {
     const reg = new RegExp(inputReg);
     if (reg.test(inputValue)) {
         el.style.border = "2px solid rgb(0, 196, 0)";
+        el.setAttribute("is-valid", "1");
     } else {
         el.style.border = "2px solid rgb(255, 0, 0)";
+        el.setAttribute("is-valid", "0");
     }
+}
+
+function buttonHandler(e) {
+  const allValid = [];
+  validFormArr.forEach((el) => {
+    allValid.push(el.getAttribute("is-valid"));
+  });
+  const isAllValid = allValid.reduce((acc, current) => {
+    return acc && current;
+  });
+ 
+  if (!Boolean(Number(isAllValid))) {
+    e.preventDefault();
+  }
 }
 
 
 
-
-
-
-
-
-
-
-
-
-
-signInBtn.addEventListener('click', function () {
-    formBox.classList.remove('active');
-    body.classList.remove('active');
-});
-signUnBtn.addEventListener('click', function () {
-    formBox.classList.add('active');
-    body.classList.add('active');
-});
 

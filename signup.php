e560430f73df3d9cb2fb639ab683a66e2a15de42
ilysenko <i.lysenko.bekey@gmail.com>
2022-06-10 10:18:15 +0300

    sanya blyat

--- signup.php
+++ signup.php
@@ -20,7 +20,7 @@ if (isset($data['signup'])) {
         $user->lastName = $data['lastName'];
         $user->email = $data['email'];
         $user->phone = $data['phone'];
-        $user->pass = password_hash($data['pass'], PASSWORD_DEFAULT);
+        $user->pass = password_hash($data['registerUserPassword'], PASSWORD_DEFAULT);
         R::store($user);
     }
 }

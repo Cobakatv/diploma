e03e373827d8ac66c2fdf6be874bca225ba9c3b3
ilysenko <i.lysenko.bekey@gmail.com>
2022-06-11 10:06:11 +0300

    pginaciya

--- catalog.php
+++ catalog.php
@@ -42,7 +42,7 @@ foreach($db_products as $row) {
                         <li class="menu_liner"><a class="menu_link" href="#">Контакты</a></li>
                         <li class="menu_liner"><a class="menu_link" href="#"><?php echo count($db_products); ?></a></li>
                         <li class="menu_liner"><a class="menu_link" href="#"><?php echo count($products); ?></a></li>
-                        <li class="menu_liner"><a class="menu_link" href="#"><?php echo $products; ?></a></li>
+                        <li class="menu_liner"><a class="menu_link" href="#"><?php echo $products[0]; ?></a></li>
                     </ul>
                     <div class="input">
                         <input class="input_text" type="text" placeholder="поиск по каталогу">

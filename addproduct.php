2cc098fd5ded205cd0a9e6f7be0c838272efdeeb
ilysenko <i.lysenko.bekey@gmail.com>
2022-06-14 20:46:34 +0300

    some

--- addproduct.php
+++ addproduct.php
@@ -1,5 +1,49 @@
 <?php 
-require 'db.php';
+    require 'db.php';
+    $data = $_POST;
+    $showError = false;
+
+    if (isset($data['submit'])) {
+        $error = array();
+        if (trim($data['modelid']) == '') {
+            $error[] = 'Укажите id товара';
+        }
+        if (trim($data['name']) == '') {
+            $error[] = 'Укажите название товара';
+        }
+        if (trim($data['price']) == '') {
+            $error[] = 'Укажите цену товара';
+        }
+        if (trim($data['price']) == '0') {
+            $error[] = 'Укажите цену товара';
+        }
+        if (trim($data['discount']) == '0') {
+            $data['discount'] = '';
+        }
+        if (trim($data['amount']) == '') {
+            $error[] = 'Укажите количество товара';
+        }
+        if (trim($data['description']) == '') {
+            $error[] = 'Укажите описание товара';
+        }
+        if (trim($data['image']) == '') {
+            $error[] = 'Укажите картинку товара';
+        }
+        if(empty($error)) {
+            $product = R::dispence('Models');
+            $product ->modelid = $data['modelid'];
+            $product ->modeltype = $data['modeltype'];
+            $product ->name = $data['name'];
+            $product ->producer = $data['producer'];
+            $product ->price = $data['price'];
+            $product ->discount = $data['discount'];
+            $product ->amount = $data['amount'];
+            $product ->description = $data['description'];
+            $product ->images = $data['image'];
+        } else {
+            $showError = True;
+        }
+    }
 
 ?>
 
@@ -20,7 +64,7 @@ require 'db.php';
     <div class="input-input">
         <form action="addproduct.php" method="POST">
             <a> Тип товара </a>
-            <input type="text" name="typeofmodel">
+            <input type="text" name="modeltype">
             <a> id товара </a>
             <input type="text" name="modelid">
             <a> Название товара </a>
@@ -38,6 +82,7 @@ require 'db.php';
             <a> Картинки </a>
             <input type="text" name="image">
             <button type="submit" name="submit"> Отправить </button>
+            <a><?php if ($showError) {echo showError($error);} ?> </a>
         </form>
 
 
